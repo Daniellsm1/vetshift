70a9a3e6a6e4dfddbbc932d0b1884aaf04dc4d7a
daniells <denniseltravieso01@gmail.com>
2026-05-03 17:06:32 +0400

    correcion de supabase

--- vetcontrol/src/pages/Login.tsx
+++ vetcontrol/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { supabase } from '../lib/supabase'
-import dogLogo from '../assets/logo.jpg'
+import dogLogo from '../assets/logo.png'
 
 export default function Login() {
   const [email, setEmail] = useState('')
